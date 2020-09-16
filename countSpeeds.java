9766cfcdf88dbb092a41765b216a53cb49bb93d3
efilnikufesin <48924713+efilnikufesin@users.noreply.github.com>
2020-09-16 10:55:31 +0300

    Create countSpeeds.java

---
+++ countSpeeds.java
@@ -0,0 +1,39 @@
+
+class countSpeeds {
+    public static void main(String[] args) {
+        Spaceship[] obj = new Spaceship[3];
+        double sum = 0;
+        String minName = "";
+        obj[0] = new Spaceship(888, "Icarus2");
+        obj[1] = new Spaceship(123, "LewisAndClark");
+        obj[2] = new Spaceship(456, "Serenity");
+        int minVal = obj[0].getSpeed();
+        for (int i = 0; i < obj.length; i++) {
+            int newSpeed = obj[i].getSpeed();
+            sum += newSpeed;
+            if(newSpeed < minVal) {
+                minVal = newSpeed;
+                minName = obj[i].getName();
+            }
+        }
+
+        double averageSpeed = sum / obj.length;
+        System.out.println("Average speed is: " + averageSpeed + " and the minimum have " + minName + " with " +minVal);
+    }
+}
+
+class Spaceship {
+    private int speed;
+    private String name;
+    public Spaceship(int speed, String name) {
+        this.speed = speed;
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    public String getName() {
+        return name;
+    }
+}
